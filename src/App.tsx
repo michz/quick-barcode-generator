5954119a9dd8b5fa4fc00d1d3300e8c0ef426378
Michael Zapf <m.zapf@mztx.de>
2023-12-26 12:53:51 +0100

    qr generation works

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,63 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useEffect, useRef, useState } from "react";
+//import { invoke } from "@tauri-apps/api/tauri";
+import { getMatches } from '@tauri-apps/api/cli'
+
+import { BrowserQRCodeSvgWriter } from "@zxing/browser";
+
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const inputElement = useRef<HTMLInputElement>(null)
+  const result = useRef<HTMLDivElement>(null)
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  async function generateCode() {
+    if (!result.current) return;
+    if (code.length === 0) return;
+
+    const codeWriter = new BrowserQRCodeSvgWriter()
+
+    result.current.innerText = '';
+    const svgElement: SVGSVGElement = codeWriter.write(code, 400, 400)
+    svgElement.setAttribute("width", "100%")
+    svgElement.setAttribute("height", "100%")
+    result.current.append(svgElement)
   }
+
+  useEffect(() => {
+    getMatches().then((matches) => {
+      if (!inputElement.current) return;
+      if ("data" in matches.args && matches.args["data"].value !== null) {
+        setCode(matches.args["data"].value.toString())
+        //generateCode()
+      }
+    })
+  }, [getMatches])
+
+  // Code changed
+  useEffect(() => {
+    generateCode()
+  }, [code])
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          //generateCode();
         }}
       >
         <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          id="code-input"
+          ref={inputElement}
+          onChange={(e) => setCode(e.currentTarget.value)}
+          value={code}
+          placeholder="Enter a code..."
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Generate</button>
       </form>
 
-      <p>{greetMsg}</p>
+      <div id="result" ref={result}></div>
     </div>
   );
 }
